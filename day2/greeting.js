c877c3cbe951da7dfe44cba387b8e7df9b2fc4dd
jasonlee <jlee7060@gmail.com>
2021-08-17 13:34:05 -0400

    aded greeting.js

---
+++ day2/greeting.js
@@ -0,0 +1,82 @@
+/*
+Author: Jason Lee 08/17/2021
+Use the strings in the arrays to form a sentence like: "Hi, my name is <Name>
+Be sure to have beginning words of sentences with the proper case.
+
+Console log the result
+
+Use a different greeting word for each gender
+Use "his" for men, "her" for women, "their" for nonbinary.
+
+Use "my" if the isSelf argument is true
+
+const greetingWords = ["hello","hi","hey"];
+const prefixes = ["my", "his", "her", "their"];
+*/
+
+/**
+ @param {string} name
+        name of person
+ @parm  {string} gender
+        Possible values of "gender" argument are "male", "female", "nonbinary"
+ @param {boolean} isSelf
+ @returns {string}
+          greeting sentence
+*/
+
+// transform only first char of string as upper case
+function firstCharUpperCase(str) {
+    str.toString().toLowerCase();
+    str = str.toString().charAt(0).toUpperCase() + str.toString().slice(1);
+    return str
+}
+
+function greeting(name, gender, isSelf) {
+    // return string var
+    let ret = [];
+
+    //     greeting msg with first char upper case only
+    let randomGreeting = greetingWords[Math.floor(Math.random() * greetingWords.length)];
+
+    randomGreeting = firstCharUpperCase(randomGreeting) + ",";
+    ret.push(randomGreeting);
+
+    //     checking name and gender with regex
+    if (!/[a-zA-Z]+/.test(name) | !genders.includes(gender)) {
+        return "Please check your name or enter the proper gender. e.g) male, female or nonbinary ";
+    }
+    // if self is true, prefix as "my"
+    if (isSelf == true) {
+        ret.push("my");
+        // if self is false, prefix as "male/female/their depends on gender param"
+    } else {
+        if (gender == "male") {
+            ret.push("his")
+        } else if (gender == "female") {
+            ret.push("her")
+                // default as "their" 
+        } else {
+            ret.push("their")
+        }
+    }
+    ret.push("name is");
+    ret.push(firstCharUpperCase(name));
+
+
+    // adding space for each str
+    ret = ret.join(" ");
+
+    return ret; // use template literal for string to return
+}
+
+// expecting hoisting
+const greetingWords = ["hello", "hi", "hey"];
+const prefixes = ["my", "his", "her", "their"];
+const genders = ["male", "female", "nonbinary"];
+
+var isSelf = false;
+console.log(greeting("jason", "male", true));
+console.log(greeting("claire", "female", false));
+console.log(greeting("Simpson", "nonbinary", false));
+// return false test
+console.log(greeting("1234", "male", true));
---
+++ day2/js-object-examples.js
@@ -0,0 +1,25 @@
+// Example of all valid JSON types
+// See https://json-schema.org/understanding-json-schema/reference/type.html
+const myInfo = {
+    name: "Jason",
+    // JSON doesn't accept date type
+    birthday: "01011990",
+    citiesLivedIn: ["Boston", "MA"],
+    isHuman: true,
+    planetsVisited: null,
+    personalInfo: {
+        favoriteFood: "pizza",
+    },
+};
+
+console.log(myInfo);
+
+const myInfoAsJSON = JSON.stringify(myInfo);
+console.log(myInfoAsJSON);
+
+function sayHi(name) {
+    return `my name is ${name} and I'm ${age} years old`;
+}
+console.log(sayHi("Jason, 30"));
+
+
